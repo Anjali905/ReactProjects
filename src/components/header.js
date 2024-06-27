9c509695160fb3e8ef8300df0a74a1253fd7ebb6
Anjali Ajith <anjusukumari@gmail.com>
2024-06-27 20:52:40 +0530

    intial commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import coffee from '../assets/coffee.jpg';
+const Header = () => {
+  return (
+    <div>
+    <img src = {coffee}></img>
+    </div>
+  )
+}
+
+export default Header
